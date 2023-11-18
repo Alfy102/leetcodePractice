eba5addc557d8b75f04cdfd94964d625b625f61d
Alfy102 <aliff102.syazwan@gmail.com>
2023-11-18 10:25:52 +0800

    Add files via upload

---
+++ Two_Sum/twoSum.py
@@ -0,0 +1,18 @@
+def twoSum(nums, target):
+    numberMap= dict(enumerate(nums))
+    possibleIndex = []
+    for i in range(len(nums)):
+        remain = target - nums[i]
+        if remain in numberMap.values():
+            for index, value in numberMap.items():
+                if index == i:
+                    continue
+                if value == remain:
+                    possibleIndex.append(i)
+    return possibleIndex
+
+        
+
+numbers = [0, 3, 2, 3, 0]
+target = 0
+print(twoSum(nums= numbers, target= target))
